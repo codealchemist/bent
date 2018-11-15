f524371366f10204ac3f9cdeb070eda564c86160
Alberto Miranda <amiranda@elementum.com>
2018-11-15 19:09:04 -0300

    Testing.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,18 @@
 const express = require('express')
-const { WebClient } = require('@slack/client')
+const { RTMClient } = require('@slack/client')
 
 const app = express()
 const port = process.env.PORT || 3000
 const token = process.env.SLACK_TOKEN || 'xoxp-142872758064-153754285521-480402916945-b8f82adecff5d64e6a4d159f2fd606f8'
-const slack = new WebClient(token)
+const slack = new RTMClient(token)
 const channel = process.env.SLACK_CHANNEL || '@bert'
 
 app.get('/topic/:topicId', (req, res) => {
   const { topicId } = req.params
   if (!topicId) return console.error('Error: No topic ID.')
 
-  slack.chat
-    .postMessage({ channel, text: `Hello there! TOPIC: ${topicId}` })
+  slack
+    .sendMessage(`Hello there! TOPIC: ${topicId}`, channel)
     .then(res => {
       // `res` contains information about the posted message
       console.log('Message sent: ', res.ts)
