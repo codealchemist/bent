dae01df02b138c5c91cc406e89f09e4ba714157b
Alberto Miranda <amiranda@elementum.com>
2018-11-15 19:38:46 -0300

    Added topics; sending messages using incoming webhooks.

--- src/index.js
+++ src/index.js
@@ -1,23 +1,37 @@
 const express = require('express')
-const { RTMClient } = require('@slack/client')
-
+const request = require('request')
+const topics = require('./topics')
 const app = express()
 const port = process.env.PORT || 3000
-const token = process.env.SLACK_TOKEN || 'xoxp-142872758064-153754285521-480402916945-b8f82adecff5d64e6a4d159f2fd606f8'
-const slack = new RTMClient(token)
-const channel = process.env.SLACK_CHANNEL || '@bert'
+const url = process.env.SLACK_URL || 'https://hooks.slack.com/services/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'
 
 app.get('/topic/:topicId', (req, res) => {
   const { topicId } = req.params
-  if (!topicId) return console.error('Error: No topic ID.')
+  if (!topicId) {
+    res.status(500).send('ERROR: No topic specified.')
+    return console.error('Error: No topic ID.')
+  }
 
-  slack
-    .sendMessage(`Hello there! TOPIC: ${topicId}`, channel)
-    .then(res => {
-      // `res` contains information about the posted message
-      console.log('Message sent: ', res.ts)
-    })
-    .catch(console.error)
+  const topic = topics[topicId]
+  if (!topic) {
+    res.status(404).send('ERROR: Topic not found.')
+    return console.error(`Error: Topic '${topicId}' not found.`)
+  }
+
+  const data = {
+    text: topic.message
+  }
+  request.post({ url, json: data }, (err, response, body) => {
+    if (err) {
+      res.status(500).send(err)
+      return console.error('Error:', err)
+    }
+
+    res.status(200).send('OK')
+    console.log(`SENT ${topicId}:`, topic.message)
+  })
 })
 
-app.listen(port)
+app.listen(port, () => {
+  console.log(`BENT listening on: http://localhost:${port}`)
+})
---
+++ src/topics.js
@@ -0,0 +1,31 @@
+const topics = {
+  t1: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *cafetería planta baja*.'
+  },
+  t2: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *cafetería 1er piso*.'
+  },
+  t3: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *cocina*.'
+  },
+  t4: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *baño hombres planta baja*.'
+  },
+  t5: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *baño mujeres planta baja*.'
+  },
+  t6: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *baño discapacitados planta baja*.'
+  },
+  t4: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *baño hombres 1er piso*.'
+  },
+  t5: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *baño mujeres 1er piso*.'
+  },
+  t6: {
+    message: 'Hola! Acaban de solicitar una revisión del sector *baño discapacitados 1er piso*.'
+  }
+}
+
+module.exports = topics
